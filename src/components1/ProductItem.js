4ac59493fc34a4512b836dbe3a8947a8892e8093
gungunsaluja <gungunsaluja16@gmail.com>
2024-03-13 19:47:18 +0530

    first commit

--- src/components1/ProductItem.js
+++ src/components1/ProductItem.js
@@ -24,4 +24,4 @@ const ProductItem = (props) =>{
 
 }
 export default ProductItem;
-//in above state class the execution or program will follow these steps first it will const [title,setTitle] first it will store the  initial value of title the which it will get from props.title  setTitle will be used to change the value of title there is a button when we click that button click event will fire and click event is mapped with onclick prop and onclick prop is mapped with handleclick function and handleclick function has setTitle event in its body ans setTitle is handling to change the title and settitle  ui par render b kar ra h
+//in above state class the execution or program will follow these steps first it will const [title,setTitle] first it will store the  initial value of title the which it will get from props.title  setTitle will be used to change the value of title there is a button when we click that button click event will fire and click event is mapped with onclick prop and onclick prop is mapped with handleclick function and handleclick function has setTitle event in its body ans setTitle is handling to change the title and settitle  ui par render b kar ra 
