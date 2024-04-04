c03cf637afb71c2c62b36c0397d05f2bae6e7680
gungunsaluja <gungunsaluja16@gmail.com>
2024-04-04 16:41:59 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-    <App />
+  <App />
   // </React.StrictMode>
 );
 
@@ -15,7 +15,14 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
-
 // React is all about components
-// sir dimag ke liye --custom creation of html is react react me sirf end state btane ki need hai
+//1--- sir dimag ke liye --custom creation of html is
+//  2--react react me sirf end state btane ki need hai in
+//3-- rwact is a single page application
+//pakagejson ase file hai jisme dependencies,versions and scripts hoti hai
+//whenever are page is loading (html file) then index.js is tha first file which will be executed
+
+// App is the first component in react
+// 
+
 reportWebVitals();
